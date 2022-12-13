4f83de8ced85df868e5c35a59f1ff32b2a0c2345
Tamir <tmelzerb@gmail.com>
2022-12-13 10:47:06 -0800

    Added script to generate directories for new days

---
+++ day.sh
@@ -0,0 +1,13 @@
+echo "Create advent of code directory for which day?"
+
+read day
+
+echo "Creating directory for day$day..."
+
+DIR=day$day
+mkdir $DIR
+cd $DIR
+echo "const tlib = require('../tlib');" > p1.js
+echo "const tlib = require('../tlib');" > p2.js
+touch input.txt
+touch input2.txt
